478e1473d7796c89afb7c1c894de9e34093f2d24
bdncode <bdn81prs@gmail.com>
2023-07-14 09:16:48 +0200

    Add DoublyLinkedList impl
    Crear implementacion DoublyLinkedList y README.md

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # capitole-consulting-test
  Prueba tecnica Capitole Consulting
+
+
+### Implementacion en Java una lista doblemente enlazada
+
+La lista implementa los siguientes métodos:
+
+- `addFirst`: Añade un elemento al principio de la lista.
+- `addLast`: Añade un elemento al final de la lista.
+- `add`: Añade un elemento en un índice específico de la lista.
+- `removeFirst`: Elimina y devuelve el primer elemento de la lista.
+- `removeLast`: Elimina y devuelve el último elemento de la lista.
+- `remove`: Elimina y devuelve el elemento en un índice específico de la lista.
+- `getFirst`: Devuelve el primer elemento de la lista.
+- `getLast`: Devuelve el último elemento de la lista.
+- `get`: Devuelve el elemento en un índice específico de la lista.
+- `contains`: Verifica si la lista contiene un elemento específico.
+- `isEmpty`: Verifica si la lista está vacía.
+- `size`: Devuelve el número de elementos en la lista.
+- `clear`: Elimina todos los elementos de la lista.
+
+y los test unitarios prueban cada uno de ellos.
+
+
+### Mejora de la clase TestSqlDao.java
+
+* Mejorar cada uno de los métodos a nivel SQL y código Java cuando sea necesario
+* Expicar y razonar cada una de las mejoras dejando comentarios en codigo
--- src/main/java/list/DoublyLinkedList.java
+++ src/main/java/list/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package list;
 
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedList<E> {
 
     private Node<E> first;
@@ -7,60 +9,161 @@ public class DoublyLinkedList<E> {
     private int size;
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element, null, first);
+        if (first != null) {
+            first.prev = newNode;
+        } else {
+            last = newNode;
+        }
+        first = newNode;
+        size++;
     }
 
     public E getFirst() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return first.data;
     }
 
     public void addLast(E element) {
+        Node<E> newNode = new Node<>(element, last, null);
+        if (last != null) {
+            last.next = newNode;
+        } else {
+            first = newNode;
+        }
+        last = newNode;
+        size++;
     }
 
     public E getLast() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return last.data;
     }
 
     public void add(E element, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(element);
+        } else if (index == size) {
+            addLast(element);
+        } else {
+            Node<E> prevNode = getNode(index - 1);
+            Node<E> nextNode = prevNode.next;
+            Node<E> newNode = new Node<>(element, prevNode, nextNode);
+            prevNode.next = newNode;
+            nextNode.prev = newNode;
+            size++;
+        }
     }
 
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> node = getNode(index);
+        return node.data;
     }
 
     public E removeFirst() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node<E> removedNode = first;
+        first = first.next;
+        if (first != null) {
+            first.prev = null;
+        } else {
+            last = null;
+        }
+        size--;
+        return removedNode.data;
     }
 
     public E removeLast() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node<E> removedNode = last;
+        last = last.prev;
+        if (last != null) {
+            last.next = null;
+        } else {
+            first = null;
+        }
+        size--;
+        return removedNode.data;
     }
 
     public E remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLast();
+        } else {
+            Node<E> removedNode = getNode(index);
+            Node<E> prevNode = removedNode.prev;
+            Node<E> nextNode = removedNode.next;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+            size--;
+            return removedNode.data;
+        }
     }
 
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) != -1;
     }
 
     public void clear() {
+        first = null;
+        last = null;
+        size = 0;
     }
 
     public int indexOf(E element) {
-        return 0;
+        Node<E> current = first;
+        int index = 0;
+        while (current != null) {
+            if (current.data.equals(element)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
     }
 
-    private class Node<E> {
+    private Node<E> getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
 
-        private E data;
+    private static class Node<E> {
+
+        private final E data;
         private Node<E> prev;
         private Node<E> next;
 
